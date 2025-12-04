64e693b300f2340668101762949caeaf5889206f
MateusTMF <matefe1200@gmail.com>
2025-12-04 09:48:26 -0300

    Atualização

--- app/producao/projetos/novo/page.tsx
+++ app/producao/projetos/novo/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { useRouter } from "next/navigation"
-import { FormCadastroProjeto } from "@/components/projeto/form-cadastro-projeto"
 import type { ProjetoTecnico } from "@/lib/types/projeto"
 import Sidebar from "@/components/sidebar"
 import { Card } from "@/components/ui/card"
+import FormCadastroProjeto from "@/components/projeto/form-cadastro-projeto"
 
 export default function NovoProjeto() {
   const router = useRouter()
